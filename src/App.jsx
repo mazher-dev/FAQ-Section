f9e670ffc4651d9292e8916de502f05a02fa8f94
MH Code <mazheriftikhar1@gmail.com>
2024-12-02 16:49:57 +0500

    2-Dec channges

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import FAQ2 from './Components/FAQ2/FAQ2.JSX'
 import FAQ3 from './Components/FAQ3/FAQ3'
 import FAQ4 from './Components/FAQ4/FAQ4'
 import FAQ5 from './Components/FAQ5/FAQ5'
+import FAQ6 from './Components/FAQ6/FAQ6'
+import FAQ7 from './Components/FAQ7/FAQ7'
 
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
       <FAQ3 />
       <FAQ4 />
       <FAQ5 />
+      <FAQ6 />
+      <FAQ7 />
     </div>
   )
 }
