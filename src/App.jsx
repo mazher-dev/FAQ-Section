3a34766966e85978873e048f7baf54fd4032ee37
MH Code <mazheriftikhar1@gmail.com>
2024-12-02 15:43:38 +0500

    2-Dec changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import FAQ1 from './Components/FAQ1/FAQ1'
+import FAQ2 from './Components/FAQ2/FAQ2.JSX'
+import FAQ3 from './Components/FAQ3/FAQ3'
+import FAQ4 from './Components/FAQ4/FAQ4'
+import FAQ5 from './Components/FAQ5/FAQ5'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <FAQ1 />
+      <FAQ2 />
+      <FAQ3 />
+      <FAQ4 />
+      <FAQ5 />
+    </div>
   )
 }
 
